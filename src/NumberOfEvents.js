f907d34b007ef6d7f880eee64b85c618211f09c7
KupferJ <jorgekupfer@gmx.de>
2022-04-24 13:57:02 +0200

    cleaned up code

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -3,26 +3,25 @@ import React, { Component } from 'react';
 class NumberOfEvents extends Component {
 
   state = {
-    numberOfEvents: '32',
-  }
+    numberOfEvents: 32,
+  };
 
   handleInputChange = (event) => {
 		const value = event.target.value;
-		this.setState({
-			numberOfEvents: event.target.value,
-		});
-	}
+		this.setState({ numberOfEvents: value });
+	};
 
   render () {
-    const { numberOfEvents } =this.state;
+    const { numberOfEvents } = this.state;
 
     return (
       <div className="eventNumber">
-        <input type="number" className="numberOfEvents" onChange={this.handleInputChange} value={this.state.numberOfEvents} />
+        <label>Number of events: </label>
+        <input type="text" className="numberOfEvents" value={numberOfEvents} onChange={this.handleInputChange} />
       </div>
-    )  
+    );
   }
 
-};
+}
 
-export default NumberOfEvents
+export default NumberOfEvents;
