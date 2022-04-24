04c1ec4a47a898ea42a42302959b679f40362b98
KupferJ <jorgekupfer@gmx.de>
2022-04-24 14:04:05 +0200

    added componentWillMount

--- src/App.js
+++ src/App.js
@@ -2,16 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList.js';
 import CitySearch from './CitySearch';
+import { getEvents, extractLocations } from './api';
+import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
+
+  state = {
+    events: [],
+    locations: []
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+  
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
   render() {
+
     return (
       <div className="App">
-        <CitySearch />
-        <EventList />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <NumberOfEvents />
+        <EventList  events={this.state.events} />
       </div>
     );
   }
+  
 }
 
 export default App;
