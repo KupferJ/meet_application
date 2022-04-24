fcb2f3ea0ed308e96ca52364c3915b0a513edeb2
KupferJ <jorgekupfer@gmx.de>
2022-04-24 21:09:15 +0200

    refactored updateEvents function

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ class App extends Component {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+        this.setState({
+        events: events.slice(0, this.state.numberOfEvents),
+        locations: extractLocations(events) });
       }
     });
   }
@@ -26,19 +28,12 @@ class App extends Component {
     this.mounted = false;
   }
 
-  updateNumberOfEvents = (numberOfEvents) => {
-    this.setState({
-      numberOfEvents,
-    },
-    this.updateEvents(this.state.locations, numberOfEvents)
-    );
-  };
-
   updateEvents = (location, eventCount) => {
     getEvents().then((events) => {
-      const locationEvents = (location === 'all')
-      ? events 
-      : events.filter((event) => event.location === location);
+      const locationEvents =
+        location === 'all'
+          ? events
+          : events.filter((event) => event.location === location);
       if (this.mounted) {
         this.setState({
           events: locationEvents.slice(0, this.state.numberOfEvents),
@@ -47,15 +42,15 @@ class App extends Component {
         });
       }
     });
-  }
-
+  };
+ 
   render() {
 
     return (
       <div className="App">
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents />
-        <EventList events={this.state.events} numberOfEvents={this.state.numberOfEvents}/>
+        <EventList className="event-list" events={this.state.events} numberOfEvents={this.state.numberOfEvents}/>
       </div>
     );
   }
