413a76b80a61056ecae4dce1b9bf761edd51d727
KupferJ <jorgekupfer@gmx.de>
2022-04-26 22:42:33 +0200

    added an InfoAlert
    added new folder Alert.js, added an Infoalert and imported it into the CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
 
   state = {
     query: '',
     suggestions: [], 
-    showSuggestions: undefined
+    showSuggestions: undefined,
+    infoText: ''
   }
 
   handleInputChanged = (event) => {
     const value = event.target.value;
+    this.setState({showSuggestions: true});
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
-    });
+    if (suggestions.length === 0) {
+      this.setState ({
+        query: value,
+        infoText: 'No information about the city you are looking for. Please check your spelling or try another city'
+      });
+    } else {
+      return this.setState({
+        query: value,
+        suggestions,
+        infoText: ''
+      });
+    }
   };
+
 
   handleItemClicked = (suggestion) => {
     this.setState({
@@ -31,6 +43,7 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
         <input
         type="text"
         className="city"
