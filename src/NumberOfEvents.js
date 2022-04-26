579b5c783d7071589db14d39c3e06e85d6c90b54
KupferJ <jorgekupfer@gmx.de>
2022-04-26 23:53:40 +0200

    added ErrorAlert
    added ErrorAlert function, imported ErrorAlert into NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,36 +1,38 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
 
   state = {
     numberOfEvents: 32,
-    infoText: "",
+    errorText: '',
   };
-
 
   handleInputChanged = (event) => {
     const number = event.target.value;
     if (number < 1 || number > 32) {
       this.setState({
         numberOfEvents: number,
-        infoText: "Enter a number between 1 and 32.",
+        errorText: 'Enter a number between 1 and 32',
       });
     } else {
       this.setState({
         numberOfEvents: number,
-        infoText: "",
+        errorText: '',
       });
     }
     this.props.updateNumberOfEvents(event.target.value);
   }
     
-
   render () {
 
     return (
       <div className="eventNumber">
         <label>Number of events: </label>
         <input type="number" className="numberOfEvents" value={this.state.numberOfEvents} onChange={this.handleInputChanged} />
+        <div className="errorAlert">
+        <ErrorAlert text={this.state.errorText} />
+        </div>  
       </div>
     );
   }
