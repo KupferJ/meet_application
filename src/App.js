5172b7338cf50b6022080256a3b530a30a68f7bb
KupferJ <jorgekupfer@gmx.de>
2022-04-27 16:08:57 +0200

    implemented offline mode

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList.js';
 import CitySearch from './CitySearch';
-import { getEvents, extractLocations } from './api';
+import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import NumberOfEvents from './NumberOfEvents';
 import { OfflineAlert } from './Alert';
+import WelcomeScreen from './WelcomeScreen'; 
+
 
 class App extends Component {
 
@@ -13,19 +15,35 @@ class App extends Component {
     locations: [],
     numberOfEvents: 32,
     location: 'all',
-    offlineText: ''
+    offlineText: '',
+    showWelcomeScreen: undefined,
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({
-        events: events.slice(0, this.state.numberOfEvents),
-        locations: extractLocations(events) });
-      }
-    });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false: true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get('code');
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
+    if (!navigator.onLine) {
+      this.setState({
+        offlineText: 'You appear to be offline. Events cannot be updated.',
+      });
+    } else {
+      this.setState({
+        offlineText: '',
+      });
+    }
   }
+
   
   componentWillUnmount(){
     this.mounted = false;
@@ -44,15 +62,6 @@ class App extends Component {
           currentLocation: location,
         });
       }
-      if (!navigator.onLine) {
-        this.setState({
-          offlineText: 'You appear to be offline. Events cannot be updated.',
-        });
-      } else {
-        this.setState({
-          offlineText: '',
-        });
-      }
     });
   };
 
@@ -67,7 +76,7 @@ class App extends Component {
 
  
   render() {
-
+    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
     return (
 
       <div className="App">
@@ -83,6 +92,9 @@ class App extends Component {
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents updateNumberOfEvents={this.updateNumberOfEvents} />
         <EventList className="event-list" events={this.state.events} numberOfEvents={this.state.numberOfEvents}/>
+
+
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
